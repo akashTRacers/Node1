eebb03d1c195523620c182c334c7f5a6920ac7fe
akash jain <akjain@grepruby.com>
2018-05-25 11:53:05 +0530

    new file test.js

---
+++ test.js
@@ -0,0 +1,2 @@
+var point = require('/home/rails/Akash/Node1');  //we simply point to the root of the local project, which will find the package.json file which in turn points to your index.js file
+point.start(); 
